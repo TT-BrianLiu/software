da9b6776dd548a37a73e7b95a163668476360a7a
Brian Liu <bliu@tenstorrent.com>
2024-07-10 18:19:36 -0400

    Add optional bash-it install instructions

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,3 +1,10 @@
+# (Optional) Install bash-it: https://github.com/Bash-it/bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh # NOTE: This overrides .bashrc by default; select Yes when it asks if you want to append instead
+# Change default theme to metal: export BASH_IT_THEME='metal'
+source ~/.bashrc
+
+
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
